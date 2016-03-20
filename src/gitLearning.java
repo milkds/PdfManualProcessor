1692fb9ef2e9201200cd6cd3386b97863637ed17
Prohorenko.ev <arockatsu@mail.ru>
2016-03-20 15:57:02 +0200

    my first commit from Idea

---
+++ src/gitLearning.java
@@ -0,0 +1,8 @@
+/**
+ * Created by AUSU on 20.03.2016.
+ */
+public class gitLearning {
+    public static void main(String[] args) {
+        System.out.println("my first github commit from Idea");
+    }
+}
