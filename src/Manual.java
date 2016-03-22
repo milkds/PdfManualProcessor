3f1af22fb0212669a4cfd75a10b0d33c1d969723
Prohorenko.ev <arockatsu@mail.ru>
2016-03-22 21:30:58 +0200

    added Manual class

---
+++ src/Manual.java
@@ -0,0 +1,14 @@
+public class Manual {
+
+    private String consoleUrl;
+    private String pdfUrl;
+    private String id;
+
+
+    public Manual(String consoleUrl, String pdfUrl) {
+        this.consoleUrl = consoleUrl;
+        this.pdfUrl = pdfUrl;
+    }
+
+    // TODO:  initialize id
+}
