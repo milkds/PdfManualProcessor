0d117db54bbd7642adf08ce9a36458d452cffa8f
dimalinc <linc@ua.fm>
2016-03-22 11:42:40 +0200

    getHtmlDocument

--- src/gitLearning.java
+++ src/gitLearning.java
@@ -1,7 +1,24 @@
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 
 public class gitLearning {
-    public static void main(String[] args) {
-        System.out.println("my first github commit from Idea");
+    static final String URL = "http://74.117.180.69:83/work/pdfapprove/index.php";
+
+    public static void main(String[] args)
+    {
+        login();
+      Document doc = new gitLearning().getHtmlDocument();
+    }
+
+
+
+    protected Document getHtmlDocument() {
+        Document doc = null;
+        try{
+          doc = Jsoup.connect(URL).get();
+        } catch (IOException e) {e.printStackTrace();}
+        return doc;
     }
 }
