f2ef22b508e8eeca31a292f0cad443910fe563cd
Prohorenko.ev <arockatsu@mail.ru>
2016-04-12 20:58:15 +0300

    ManualPageParser comments added

--- src/PdfManualProcessor/service/ManualPageParser.java
+++ src/PdfManualProcessor/service/ManualPageParser.java
@@ -1,7 +1,7 @@
 package PdfManualProcessor.service;
 
 /**
- * This class gets HTML page and Parses it on up to 10 Manuals
+ * This class gets HTML page from LoginHandler and Parses it on up to 10 Manuals. Returns List<Manual> manuals.
  */
 public class ManualPageParser {
 
