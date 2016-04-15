412251e33b82295ee7276008ba8e4f1992823811
Prohorenko.ev <arockatsu@mail.ru>
2016-04-15 17:39:47 +0300

    Implemented ManualSizeChecker

--- src/PdfManualProcessor/service/ManualSizeChecker.java
+++ src/PdfManualProcessor/service/ManualSizeChecker.java
@@ -2,14 +2,21 @@ package PdfManualProcessor.service;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPReply;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ManualSizeChecker {
-
 
     public static int getSizeByFtp(String urlString){
         int result =0;
@@ -31,11 +38,45 @@ public class ManualSizeChecker {
         }
         return result;
     }
-    public static int getSizeByHttp(String url){
+    public static int getSizeByHttp(HttpResponse response){
         int result =0;
+        Header header = response.getFirstHeader("Content-Length");
+        if(header!=null){
+            result = Integer.parseInt(header.getValue());
+        }
         return result;
     }
-    public static URL getUrlForFtp(String urlString) throws MalformedURLException {
+    private static HttpResponse getHttpResponse(String urlString){
+        final CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpResponse response;
+        final String checkedUrl = getUrlForHttp(urlString);
+        try {
+             HttpHead httpHead = new HttpHead(checkedUrl);
+             response = httpclient.execute(httpHead, new ResponseHandler<HttpResponse>() {
+                @Override
+                public HttpResponse handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+                    int statusCode = response.getStatusLine().getStatusCode();
+                    if (statusCode==405){
+                        response = httpclient.execute(new HttpGet(checkedUrl));
+                    }
+                    if (statusCode==404||statusCode==403||!response.containsHeader("Content-Length")){
+                        response = getHttpResponseByProxy(checkedUrl);
+                    }
+                    return response;
+                }
+            });
+        }
+        catch (IOException e) {
+           response = getHttpResponseByProxy(checkedUrl);
+        }
+        
+        return response;
+    }
+    private static HttpResponse getHttpResponseByProxy(String urlString){
+
+        return null;
+    }
+    private static URL getUrlForFtp(String urlString) throws MalformedURLException {
         urlString = urlString.replaceAll("%20"," ");
         urlString = urlString.replaceAll("%13","\n");
         urlString = urlString.replaceAll("%26","&");
@@ -44,4 +85,13 @@ public class ManualSizeChecker {
         urlString = urlString.replaceAll("%2B","+");
         return new URL(urlString);
     }
+    private static String getUrlForHttp(String urlString)  {
+        urlString = urlString.replaceAll("\\{","%7B");
+        urlString = urlString.replaceAll("\\}","%7D");
+        urlString=urlString.replaceAll("\\\\","/");
+        return urlString;
+    }
+
+
+    // TODO: 15.04.2016 implement getHttpResponseByProxy() method. Check ResponseHandler for notOpen Manuals(404 answer). Decide where to check is Manual size legit or not.
 }
