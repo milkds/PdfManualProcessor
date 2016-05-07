5b94729d47e75b5a6fd474985866dfdb5095ea00
Prohorenko.ev <arockatsu@mail.ru>
2016-05-07 15:22:48 +0300

    started Main window.

--- src/PdfManualProcessor/view/MainView.java
+++ src/PdfManualProcessor/view/MainView.java
@@ -1,10 +1,47 @@
 package PdfManualProcessor.view;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class MainView extends JPanel implements View {
+public class MainView extends JFrame implements View {
+
+    public MainView() {
+        super();
+    }
 
     @Override
     public void init() {
+
+
+        setSize(500,500);
+        setPreferredSize(new Dimension(500,500));
+        Container pane = getContentPane();
+
+        GroupLayout groupLayout = new GroupLayout(pane);
+        pane.setLayout(groupLayout);
+        groupLayout.setAutoCreateGaps(true);
+        GroupLayout.Group horButtonGroup = groupLayout.createParallelGroup();
+        GroupLayout.Group verButtonGroup = groupLayout.createSequentialGroup();
+        groupLayout.setHorizontalGroup(horButtonGroup);
+        groupLayout.setVerticalGroup(verButtonGroup);
+
+        JButton button = new JButton("Download manuals");
+        JButton button2 = new JButton("Filter manuals");
+
+        horButtonGroup.addComponent(button);
+        horButtonGroup.addComponent(button2);
+        verButtonGroup.addComponent(button);
+        verButtonGroup.addComponent(button2);
+
+        pack();
+
+
+       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+       setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new MainView().init();
     }
 }
