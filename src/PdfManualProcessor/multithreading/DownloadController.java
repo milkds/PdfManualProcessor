fed31212833a5ffc294876141e3e3818160148e7
Prohorenko.ev <arockatsu@mail.ru>
2016-06-25 12:43:16 +0300

    Commented DownloadController.class

--- src/PdfManualProcessor/multithreading/DownloadController.java
+++ src/PdfManualProcessor/multithreading/DownloadController.java
@@ -8,36 +8,58 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * This class implements manual downloading on multithreading level.
+ */
+
 public class DownloadController {
     private BlockingQueue<Manual> downloadingQueue = new LinkedBlockingQueue<>();
-    private  AtomicInteger counter;
+    private AtomicInteger counter;
     private Integer total;
-    public void downloadManuals(){
-        List<Manual> manuals = ManualSerializer.getManualsForDownload();
-        total = manuals.size();
-        counter = new AtomicInteger(0);
 
+    /**
+     * Starts threads which take manuals from downloadingQueue and download them.
+     */
+    public void downloadManuals(){
+        //getting list of manuals to download.
+        List<Manual> manuals = ManualSerializer.getManualsForDownload();
+        //initialising total count of manuals to download (needed for user notification).
+        total = manuals.size();
+        //initialising counter of downloaded manuals. (needed for user notification).
+        counter = new AtomicInteger(0);
+        //filling downloadingQueue with manuals to download
         for (Manual m : manuals){
             try {
                 downloadingQueue.put(m);
-                System.out.println(m.getId());
             } catch (InterruptedException ignored) {
             }
         }
+        //starting threads for downloading manuals. 10 seems to be optimal number, as download performed from different servers.
         for (int i = 0; i <10; i++) {
             new Thread(new ManualDownloaderNew(downloadingQueue,counter, total)).start();
         }
     }
-
+    /**
+     * Cancels download by clearing queue (some time needed for each
+     * thread to complete its current task - than it is shutdown by itself after getting null manual from queue).
+     */
     public void cancelDownload(){
         downloadingQueue.clear();
     }
 
+    /**
+     * @return Counter of downloaded manuals (for user notification).
+     */
     public AtomicInteger getCounter() {
         return counter;
     }
 
+    /**
+     * @return Total count of manuals to download (for user notification).
+     */
     public Integer getTotal() {
         return total;
     }
+
+    //todo: decide necessary number of threads for downloading and way this number stated (properties or hardcode).
 }
